c50246b694ba0917b56399f4e180d34ff29f9989
Andrew T <andrew.tran.438@my.csun.edu>
2018-09-19 10:07:21 -0700

    started project2

---
+++ comp482/Project2.java
@@ -0,0 +1,18 @@
+//comp 482 project 2
+//due:
+//Andrew Tran
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Project2{
+
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(new File("input2.txt"));
+
+		//read in the matrix
+
+
+	}//end main
+}//end class
