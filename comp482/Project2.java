11f3640d433d5355bd612fd0a6706961cff650e9
Andrew T <andrew.tran.438@my.csun.edu>
2018-10-02 15:50:42 -0700

    finally got it to store matrix

--- comp482/Project2.java
+++ comp482/Project2.java
@@ -10,15 +10,41 @@ import java.io.IOException;
 
 public class Project2{
 
+	public static int getMatrixSize() throws IOException{
+		Scanner scanLine = new Scanner(new File("input2.txt"));
+		return scanLine.nextLine().length()/2 + 1;
+	}
+
 	public static void main(String[] args) throws IOException{
 
-		// init scanner for input file
-		Scanner scanner = new Scanner(new File("input2.txt"));
+		Scanner sc = new Scanner(new File("input2.txt"));
 
-		// create a 2d array
-		int list[][] = new int[][];
+		int size = getMatrixSize();
 
-		// read from file
+		//init array
+		int list[][] = new int[size][size];
+
+		//read from file
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				list[i][j] = sc.nextInt();
+			}
+		}
+
+		//get the size of the independent set
+		int sizeOfIndSet = sc.nextInt();
+
+
+		//TEST
+		System.out.println("length of first line is "+ size );
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				System.out.print(list[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("independent set size : " + sizeOfIndSet);
 
 	}//end main
 }//end class
