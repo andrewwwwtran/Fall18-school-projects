ee5fffef414b1529a5c73f89d72b1e6d27636bbf
Andrew T <andrew.tran.438@my.csun.edu>
2018-10-03 09:56:52 -0700

    updated p2

--- comp482/Project2.java
+++ comp482/Project2.java
@@ -15,11 +15,25 @@ public class Project2{
 		return scanLine.nextLine().length()/2 + 1;
 	}
 
+	public static int[] checkArray(int array[], int k, int l){
+		for(int i : array){
+			if(i == k){
+				//dont add into array
+			}
+			else{
+				//its not in the array, add it
+
+			}
+		}
+	}
+
 	public static void main(String[] args) throws IOException{
 
 		Scanner sc = new Scanner(new File("input2.txt"));
 
 		int size = getMatrixSize();
+
+		// storeMatrix(size);
 
 		//init array
 		int list[][] = new int[size][size];
@@ -34,17 +48,46 @@ public class Project2{
 		//get the size of the independent set
 		int sizeOfIndSet = sc.nextInt();
 
+		// bruteforce
+		// idea: search array til hit 0, save index of i, j
+		// then search j, i if theres a 0
+		// if so then add to another num to compare to sizeOfIndSet
+		// continue search from i, j
+		// going to add 2 times bc once it hits j, i its gonna check i, j then add
+
+		int testSize=0;
+		int arraySet[] = new int[];
+
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				if(list[i][j] == 0 && list[j][i] == 0){
+					//add 2 vertices
+					checkArray(arraySet, i, j);
+					//mark them
+					list[i][j] = -1;
+					list[j][i] = -1;
+				}
+			}
+		}
+		//compare to given number
+		if(testSize >= sizeOfIndSet){
+			System.out.println("Yes.");
+		}
+		if(testSize < sizeOfIndSet){
+			System.out.println("No.");
+		}
 
 		//TEST
 		System.out.println("length of first line is "+ size );
+		System.out.println("max ind set size: " + testSize);
 		for(int i = 0; i < size; i++){
 			for(int j = 0; j < size; j++){
 				System.out.print(list[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();
-		System.out.println("independent set size : " + sizeOfIndSet);
+
+		// System.out.println("independent set size : " + sizeOfIndSet);
 
 	}//end main
 }//end class
