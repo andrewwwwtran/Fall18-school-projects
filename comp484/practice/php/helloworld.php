d50a1c6cd7d52494eceedae4a8065d19b2ceca40
Andrew T <andrew.tran.438@my.csun.edu>
2018-12-06 09:56:17 -0800

    comp 484 12.05.18 update

---
+++ comp484/practice/php/helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>My First PHP Page</h1>
+		<?php
+		echo "Hello World";
+		print("<p>Hello World</p>");
+		?>
+	</body>
+</html>
--- comp484/practice/scripts/nov28.js
+++ comp484/practice/scripts/nov28.js
@@ -10,59 +10,102 @@ var value;
 window.addEventListener("load", start, false);
 
 function start(){
+
+	var saveButton = document.getElementById("saveButton");
+	var clearButton = document.getElementById("clearButton");
+
+	saveButton.addEventListener("click", saveSearch, false);
+	clearButton.addEventListener("click", clearAllSearches, false);
+
+	loadSearches();
+}
+
+function loadSearches(){
+	testStr = "";
+	markup = "";
+
 	if(!sessionStorage.getItem("herePreviously")){
 		document.getElementById("welcomeMessage").innerHTML = "Welcome to favorite twitter"
 		sessionStorage.setItem("herePreviously", "true");
 	}
 
-	// check for items
-	testStr += "<p>session storage content</p>";
-	for(let i = 0; i < sessionStorage.length; i++){
-		// sessionStorage.getItem(sessionStorage.key(i));
-		// for cleaner code do
-		key = sessionStorage.key(i);
-		value = sessionStorage.getItem(key);
-		testStr += "<p> key : " + key + ", value : " + value + ".</p>";
-	}
-
-	testStr += "<p>local storage content</p>";
-	for(let i = 0; i < localStorage.length; i++){
-		// localStorage.getItem(localStorage.key(i));
-		// for cleaner code do
-		key = localStorage.key(i);
-		value = localStorage.getItem(key);
-		testStr += "<p> key : " + key + ", value : " + value + ".</p>";
-	}
+	// // check for items
+	// testStr += "<p>session storage content</p>";
+	// for(let i = 0; i < sessionStorage.length; i++){
+	// 	// sessionStorage.getItem(sessionStorage.key(i));
+	// 	// for cleaner code do
+	// 	key = sessionStorage.key(i);
+	// 	value = sessionStorage.getItem(key);
+	// 	testStr += "<p> key : " + key + ", value : " + value + ".</p>";
+	// }
+	//
+	// testStr += "<p>local storage content</p>";
+	// for(let i = 0; i < localStorage.length; i++){
+	// 	// localStorage.getItem(localStorage.key(i));
+	// 	// for cleaner code do
+	// 	key = localStorage.key(i);
+	// 	value = localStorage.getItem(key);
+	// 	testStr += "<p> key : " + key + ", value : " + value + ".</p>";
+	// }
 
 	tags = [];
 	for(let i = 0; i < localStorage.length; i++){
 		tags[i] = localStorage.key(i);
 	}
 	tags.sort();
-	testStr += "<p>"+tags.join()+"</p>";
+	// testStr += "<p>tag array is: "+tags.join()+"</p>";
 
-	test = document.getElementById("tests"),innerHTML = testStr;
+	document.getElementById("tests").innerHTML = testStr;
 
-	output = document.getElementById("searches").innerHTML = markup;
-	output.innerHTML = markup;
-}
+	document.getElementById("searches").innerHTML = markup;
 
-function loadSearches(){
+	var tb = "";
+	var url = "https://twitter.com/search?q=";
+	var listmarkup = "<ul>";
+	for(tag in tags){
+		var query = localStorage.getItem(tags[tag]);
+		//replace spaces with '+'
+		query = query.replace(" ", "%2B");
+		query += url + query;
+		listmarkup += "<li> <a href='" + query + "'>" + tags[tag] + "</a>" +
+		"<input id = '" + tags[tag] + "' type = 'button' value = 'Edit' onclick = 'editTag(id)'>" +
+		"<input id = '" + tags[tag] + "' type = 'button' value = 'Delete' onclick = 'deleteTag(id)'</li>";
+	}
 
+	listmarkup += "</ul>";
+
+	document.getElementById("searches").innerHTML += listmarkup;
 }
 
 function clearAllSearches(){
 
+	//clear the local storage
+	localStorage.clear();
+
+	loadSearches();
 }
 
 function saveSearch(){
+	//get user entries
+	var query1 = document.getElementById("query").value;
+	var tag1 = document.getElementById("tag").value;
+	localStorage.setItem(tag1, query1);
 
+	loadSearches();
 }
 
 function deleteTag(tag){
-
+	//id is set to tags[tag]
+	//want to delete where key name is tag
+	localStorage.removeItem(tag);
+	loadSearches();
 }
 
 function editTag(tag){
+	//find the element to edit, show it where the user saves
+	document.getElementById("tag").value = tag;
+	document.getElementById("query").value = localStorage.getItem(tag);
+	//doing this will overwrite "edit" the user queries
 
+	loadSearches();
 }
