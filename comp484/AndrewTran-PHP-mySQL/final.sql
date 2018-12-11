5488c9e212edd4dea9308af518e9283e1cd3d0e7
Andrew T <andrew.tran.438@my.csun.edu>
2018-12-10 16:19:22 -0800

    comp 484 extra credit v0.1

---
+++ comp484/AndrewTran-PHP-mySQL/final.sql
@@ -0,0 +1,25 @@
+-- username: webphp, password: password, this is user to connect to db
+DROP DATABASE IF EXISTS finaldb;
+
+CREATE DATABASE finaldb;
+
+USE final;
+
+CREATE TABLE auth
+(
+	userID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username varchar(60),
+	password varchar(60),
+	firstName varchar(60),
+	lastName varchar(60),
+	email varchar(60),
+	phone varchar(60)
+);
+
+-- insert 3 records
+INSERT INTO auth (username, password, firstName, lastName, email, phone)
+VALUES ('username1', 'password1', 'Andrew', 'Proton', 'andrew.proton@gmail.com', '(123)456-7890)');
+INSERT INTO auth (username, password, firstName, lastName, email, phone)
+VALUES('username2', 'password2', 'Bob', 'Electron', 'bob.electron@gmail.com', '(321)654-0987');
+INSERT INTO auth (username, password, firstName, lastName, email, phone)
+VALUES('username3', 'password3', 'Carl', 'Neutron', 'carl.neutron@gmail.com', '(321)321-4214');
