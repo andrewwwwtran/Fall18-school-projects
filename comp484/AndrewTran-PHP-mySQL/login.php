9363508fe38dbaa7a681499580506ca8171e3fb5
Andrew T <andrew.tran.438@my.csun.edu>
2018-12-15 13:48:03 -0800

    login php update

--- comp484/AndrewTran-PHP-mySQL/login.php
+++ comp484/AndrewTran-PHP-mySQL/login.php
@@ -6,8 +6,30 @@
 	</head>
 	<body>
 		<?php
+			// make a connection to the db
+			$connect_handle = mysqli_connect("localhost", "root", "password");
+
+			if(!$connect_handle){
+				die("connection failed" . mysqli_connect_error());
+			}
+
+			// select the database
+			mysqli_select_db($connect_handle, "finaldb");
+
+			// retrieve username and password
 			$username = $_POST['username'];
 			$password = $_POST['password'];
+
+			$password_check = ("SELECT password from auth WHERE username = $username");
+
+			// check the password of that username
+			if($password == $password_check){
+				// success
+				echo "Welcome back $username! Login was successful";
+				// after login, display user's firstname, lastname, email, and phone in a table with headers.
+			}else{
+				echo "Incorrect username or password";
+			}
 
 		?>
 	</body>
