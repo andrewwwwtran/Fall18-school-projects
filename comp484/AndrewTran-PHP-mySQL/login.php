a7e1a9d037714add02b39b2d77db9707297a9d29
Andrew T <andrew.tran.438@my.csun.edu>
2018-12-15 21:57:51 -0800

    finished login But its not using the foreach to go throuhg the array

--- comp484/AndrewTran-PHP-mySQL/login.php
+++ comp484/AndrewTran-PHP-mySQL/login.php
@@ -7,7 +7,7 @@
 	<body>
 		<?php
 			// make a connection to the db
-			$connect_handle = mysqli_connect("localhost", "root", "password");
+			$connect_handle = mysqli_connect("localhost", "webphp", "password");
 
 			if(!$connect_handle){
 				die("connection failed" . mysqli_connect_error());
@@ -20,13 +20,39 @@
 			$username = $_POST['username'];
 			$password = $_POST['password'];
 
-			$password_check = ("SELECT password from auth WHERE username = $username");
+			$pw_query = ("SELECT password from auth WHERE username = '$username'");
+			$result = mysqli_query($connect_handle, $pw_query);
 
 			// check the password of that username
-			if($password == $password_check){
-				// success
-				echo "Welcome back $username! Login was successful";
-				// after login, display user's firstname, lastname, email, and phone in a table with headers.
+			if(mysqli_num_rows($result) > 0){
+				while($row = mysqli_fetch_assoc($result)){
+					// while loop should only check once because the mysql query returns only the password of that username
+					// fetch the password from the database .
+					$password_from_db = $row['password'];
+					if($password == $password_from_db){
+						// success
+						echo "Welcome back $username! Login was successful";
+						// after login, display user's firstname, lastname, email, and phone in a table with headers.
+						$user_query = ("SELECT firstname, lastname, email, phone FROM auth WHERE username = '$username'");
+						$user_result = mysqli_query($connect_handle, $user_query);
+						echo "<table>
+						<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						</tr>";
+						while($row = mysqli_fetch_assoc($user_result)){
+							echo "<tr>";
+							echo "<td>" . $row['firstname'] . "</td>";
+							echo "<td>" . $row['lastname'] . "</td>";
+							echo "<td>" . $row['email'] . "</td>";
+							echo "<td>" . $row['phone'] . "</td>";
+							echo "</tr>";
+						}
+						echo "</table>";
+					}
+				}
 			}else{
 				echo "Incorrect username or password";
 			}
